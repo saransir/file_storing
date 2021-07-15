4c082a9d8f4b1ba387e35698c9a241bca3305f98
Dᴀʀᴋ_Aɴɢᴇʟ <84849690+DarkAngel1234-tech@users.noreply.github.com>
2021-07-15 17:36:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,11 +94,11 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel to use me\n\nFirst join My Channel.</b>"
+    text = "<b>You Need To Join in My Channel To Use Me\n\First join My Channel</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f"\n<b>Then <a href='https://t.me/{client.username}?start={argument}'>CLICK HERE</a></b>"
+        text = text + f"<b>and <a href='https://t.me/{client.username}?start={argument}'>TRY AGAIN</a></b>"
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
